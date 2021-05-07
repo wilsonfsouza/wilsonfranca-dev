7e59ae5d862fea0b789709be8449afbfa9a1dec7
Wilson Franca <wilson.franca.92@gmail.com>
2021-05-06 23:47:47 -0700

    feat: added conditional rendering from horizontal to button and a drawer menu on tablets and mobile devices, making the header responsive.

--- src/components/Header/NavBar.tsx
+++ src/components/Header/NavBar.tsx
@@ -1,25 +1,71 @@
-import { HStack } from '@chakra-ui/react';
-import { ActiveLink } from './ActiveLink';
-import { NavLink } from './NavLink';
+import {
+    Drawer,
+    Icon,
+    useBreakpointValue,
+    DrawerCloseButton,
+    DrawerContent,
+    DrawerOverlay,
+    DrawerHeader,
+    DrawerBody,
+    useDisclosure,
+    IconButton
+} from '@chakra-ui/react';
+
+import { useRouter } from 'next/router';
+import { useEffect, useRef } from 'react';
+
+import { BiMenuAltRight } from 'react-icons/bi';
+import { NavList } from './NavList';
 
 export function NavBar() {
+    const navBarButtonRef = useRef(null);
+
+    const isDrawerNavBar = useBreakpointValue({
+        base: true,
+        lg: false
+    });
+
+    const { isOpen, onOpen, onClose } = useDisclosure();
+
+    const router = useRouter();
+
+    useEffect(() => {
+        onClose()
+    }, [router.asPath])
+
+    if (isDrawerNavBar) {
+        return (
+            <>
+                <IconButton
+                    ref={navBarButtonRef}
+                    aria-label="Open Navigation Menu"
+                    icon={<Icon as={BiMenuAltRight} fontSize={32} />}
+                    variant="unstyled"
+                    _hover={{
+                        color: "pink.600"
+                    }}
+                    _active={{
+                        backgroundColor: "transparent"
+                    }}
+                    onClick={onOpen}
+                />
+
+                <Drawer isOpen={isOpen} placement="left" onClose={onClose}>
+                    <DrawerOverlay>
+                        <DrawerContent bg="gray.900" p="4">
+                            <DrawerCloseButton mt="6" />
+                            <DrawerHeader fontFamily="Roboto Slab" fontWeight="bold">Menu</DrawerHeader>
+                            <DrawerBody>
+                                <NavList direction="column" />
+                            </DrawerBody>
+                        </DrawerContent>
+                    </DrawerOverlay>
+                </Drawer>
+            </>
+        )
+    }
+
     return (
-        <HStack
-            as="nav"
-            spacing="8"
-        >
-            <ActiveLink href="/" activeClassName="active">
-                <NavLink>HOME</NavLink>
-            </ActiveLink>
-            <ActiveLink href="/whoiam" activeClassName="active">
-                <NavLink>WHO I AM</NavLink>
-            </ActiveLink>
-            <ActiveLink href="/portfolio" activeClassName="active">
-                <NavLink>PORTFOLIO</NavLink>
-            </ActiveLink>
-            <ActiveLink href="/contact" activeClassName="active">
-                <NavLink>CONTACT</NavLink>
-            </ActiveLink>
-        </HStack >
+        <NavList />
     );
 }
