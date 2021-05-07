1ecbc9aae96c42a64675e83d152956151b462d5b
Wilson Franca <wilson.franca.92@gmail.com>
2021-05-06 21:30:14 -0700

    feat(Header): created header navbar - not responsive yet.

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,30 @@
+import { Flex, HStack, Image } from '@chakra-ui/react';
+import { NavBar } from './NavBar';
+
+export function Header() {
+    return (
+        <Flex
+            as="header"
+            w="100%"
+            h="36"
+            align="center"
+            backgroundColor="gray.900"
+            px="2.6875rem"
+            py="2.1875rem"
+        >
+            <HStack
+                as="div"
+                w="100%"
+                mx="auto"
+                my="0"
+                maxWidth={1480}
+            >
+                <Image w={["11.875rem", "23.275rem"]} src="/logo.svg" alt="wilsonfranca.dev" />
+
+                <Flex as="div" align="center" style={{ marginLeft: "auto" }}>
+                    <NavBar />
+                </Flex>
+            </HStack>
+        </Flex>
+    )
+}
