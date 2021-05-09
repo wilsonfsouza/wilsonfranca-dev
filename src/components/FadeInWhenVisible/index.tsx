0184605aa6a670b092b962b79a1f2d63d0705fe4
Wilson Franca <wilson.franca.92@gmail.com>
2021-05-09 12:30:14 -0700

    refactor: extracted animation container into a separate component.

---
+++ src/components/FadeInWhenVisible/index.tsx
@@ -0,0 +1,35 @@
+import { motion, useAnimation, MotionProps } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+import { ReactNode, useEffect } from "react";
+
+interface FadeInWhenVisibleProps extends MotionProps {
+    children: ReactNode;
+}
+
+export function FadeInWhenVisible({ children, ...rest }: FadeInWhenVisibleProps) {
+    const controls = useAnimation();
+    const [ref, inView] = useInView();
+
+    useEffect(() => {
+        if (inView) {
+            controls.start("visible");
+        }
+    }, [controls, inView]);
+
+    return (
+        <motion.div
+            ref={ref}
+            animate={controls}
+            initial="hidden"
+            transition={{ duration: 0.5 }}
+            variants={{
+                visible: { opacity: 1, scale: 1 },
+                hidden: { opacity: 0, scale: 0.9 }
+            }}
+            style={{ width: '100%' }}
+            {...rest}
+        >
+            {children}
+        </motion.div>
+    );
+}
