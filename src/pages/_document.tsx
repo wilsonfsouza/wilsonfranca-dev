0e87abffc1813351627b0c7f9e042f7b2463adc2
Wilson Franca <wilson.franca.92@gmail.com>
2021-05-09 21:54:45 -0700

    feat: added link to favicon.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,7 @@ export default class MyDocument extends Document {
                 <Head>
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
                     <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@300;400;500;700&family=Roboto:wght@300;500&display=swap" rel="stylesheet" />
+                    <link rel="shortcut icon" type="image/png" href="/favicons/logo-short-pink.png" />
                 </Head>
                 <body>
                     <Main />
