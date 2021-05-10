c67c2a3403ea5aaa93289a84943b29b431994446
Wilson Franca <wilson.franca.92@gmail.com>
2021-05-09 18:28:01 -0700

    feat: added section transition components to homepage.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,18 @@
+import { Flex, Heading, HStack, Text, VStack } from "@chakra-ui/react";
 import { Hero } from "../components/Hero";
+import { Section } from "../components/Section";
+import { TransitionSection } from "../components/TransitionSection";
+import { FaCode, FaChartBar, FaUserAstronaut } from 'react-icons/fa';
+import { Card } from "../components/Card";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <TransitionSection gradientDirection="normal" isBellowHero={true} />
+
+
+      <TransitionSection gradientDirection="upsidedown" />
     </>
   )
 }
