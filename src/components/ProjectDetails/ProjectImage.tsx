47fb3bfd11710a54c7837effb592745285727cdd
Wilson Franca <wilson.franca.92@gmail.com>
2021-05-18 00:03:06 -0700

    feat: isolated styling for project details content image.

---
+++ src/components/ProjectDetails/ProjectImage.tsx
@@ -0,0 +1,23 @@
+import { Box } from "@chakra-ui/react";
+import { motion } from "framer-motion";
+
+interface ProjectImageProps {
+    layoutId: string;
+    imageSrc: string;
+}
+
+export function ProjectImage({ layoutId, imageSrc }: ProjectImageProps) {
+    return (
+        <Box
+            as={motion.div}
+            layoutId={layoutId}
+            backgroundImage={`url(${imageSrc})`}
+            backgroundPosition='center'
+            backgroundSize='cover'
+            backgroundRepeat='no-repeat'
+            width='100%'
+            height='30rem'
+            borderRadius='0.625rem'
+        />
+    );
+}
