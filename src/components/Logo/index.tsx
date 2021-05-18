f6a55596127d6633743ee0b109911d38c0008644
Wilson Franca <wilson.franca.92@gmail.com>
2021-05-18 00:27:49 -0700

    feat: allowed logo to be clickable when desired.

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,6 +1,16 @@
 import { Image } from "@chakra-ui/image";
 
-export function Logo() {
+interface LogoProps {
+    isClickable?: boolean;
+    onClickFunct?: () => void;
+}
+
+export function Logo({ onClickFunct, isClickable = false }: LogoProps) {
+    if (isClickable) {
+        return (
+            <Image w={["9.9375rem", "11.375rem", "12.8125rem"]} src="/logo-full.svg" alt="wilfranca" onClick={onClickFunct} style={{ cursor: 'pointer' }} />
+        );
+    }
     return (
         <Image w={["9.9375rem", "11.375rem", "12.8125rem"]} src="/logo-full.svg" alt="wilfranca" />
     );
