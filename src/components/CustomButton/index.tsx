f9a11543b9c8579ec51447a3422739593a8f138a
Wilson Franca <wilson.franca.92@gmail.com>
2021-05-17 23:58:50 -0700

    feat: added a variation styling for secondary buttons.

--- src/components/CustomButton/index.tsx
+++ src/components/CustomButton/index.tsx
@@ -1,11 +1,29 @@
 import { Button, ButtonProps } from "@chakra-ui/button";
 import { ReactNode } from "react";
+import { FaTruckMonster } from "react-icons/fa";
 
 interface CustomButtonProps extends ButtonProps {
     children: ReactNode;
+    isPrimary?: boolean;
 }
 
-export function CustomButton({ children, ...rest }: CustomButtonProps) {
+export function CustomButton({ children, isPrimary = true, ...rest }: CustomButtonProps) {
+    if (!isPrimary) {
+        return (
+            <Button
+                variant="unstyled"
+                bg="#999591"
+                _hover={{ filter: 'brightness(0.95)' }}
+                w={["12.5rem", "15.625rem"]}
+                h="3.125rem"
+                fontSize="1rem"
+                fontWeight="medium"
+                {...rest}
+            >
+                {children}
+            </Button>
+        );
+    }
     return (
         <Button
             variant="unstyled"
