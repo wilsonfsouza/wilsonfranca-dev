8ec9161b4c15c0fbb8dd94e28cd64866fdb9c72f
Wilson Franca <wilson.franca.92@gmail.com>
2021-05-18 18:33:06 -0700

    chore: added base storybook.

---
+++ .storybook/config.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { configure, addParameters, addDecorator } from '@storybook/react';
+
+import { withInfo } from '@storybook/addon-info';
+import centered from '@storybook/addon-centered/react';
+
+import GlobalStyle from '../src/styles/theme'
