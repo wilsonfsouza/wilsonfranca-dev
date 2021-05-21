355dc626aa4e78e48bd1ff2ce225c54ccaf6917c
Wilson Franca <wilson.franca.92@gmail.com>
2021-05-20 21:20:49 -0700

    refactor: extracted the logo section of the footer into a separate component.

---
+++ src/components/Footer/LogoDivider.tsx
@@ -0,0 +1,11 @@
+import { HStack, Divider } from "@chakra-ui/react";
+import { Logo } from "../Logo";
+
+export function LogoDivider() {
+    return (
+        <HStack spacing={["1.5rem", "2.8125rem"]} w="100%" alignItems="center" justifyContent="center" pb="3.625rem">
+            <Logo />
+            <Divider orientation="horizontal" height="2px" minWidth="10px" flex="1" color="gray.500" />
+        </HStack>
+    );
+}
