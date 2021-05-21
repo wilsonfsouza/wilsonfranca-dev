31da53815a362490ce798e8efc0b896c49a45e42
Wilson Franca <wilson.franca.92@gmail.com>
2021-05-20 21:40:45 -0700

    refactor: replaced Stack with Wrap in What I Do Section.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Stack, SimpleGrid, Box, VStack } from "@chakra-ui/react";
+import { SimpleGrid, Box, VStack, Wrap, WrapItem } from "@chakra-ui/react";
 import { FaCode, FaChartBar, FaUserAstronaut } from 'react-icons/fa';
 import { Hero } from "../components/Hero";
 import { TransitionSection } from "../components/TransitionSection";
@@ -22,35 +22,41 @@ export default function Home() {
       <Hero />
       <TransitionSection gradientDirection="normal" isBellowHero={true} />
       <Section title="What I Do">
-        <Stack
-          direction={["column", "column", "row", "row"]}
+        <Wrap
           w="100%"
           spacing="2.125rem"
           alignItems="center"
-          justifyContent="space-around"
+          justifyContent="space-evenly"
         >
-          <FadeInWhenVisible>
-            <Card
-              icon={FaCode}
-              title="Design + Development"
-              description="Modern experiences optimized for performance, search engines, and converting users to customers."
-            />
-          </FadeInWhenVisible>
-          <FadeInWhenVisible>
-            <Card
-              icon={FaChartBar}
-              title="Data Analysis"
-              description="Modern experiences optimized for performance, search engines, and converting users to customers."
-            />
-          </FadeInWhenVisible>
-          <FadeInWhenVisible>
-            <Card
-              icon={FaUserAstronaut}
-              title="Friendly Interfaces"
-              description="Friendly and accessible interfaces focused on user experience that adapts to all devices."
-            />
-          </FadeInWhenVisible>
-        </Stack>
+          <WrapItem>
+            <FadeInWhenVisible>
+              <Card
+                icon={FaCode}
+                title="Design + Development"
+                description="Modern experiences optimized for performance, search engines, and converting users to customers."
+              />
+            </FadeInWhenVisible>
+          </WrapItem>
+          <WrapItem>
+            <FadeInWhenVisible>
+              <Card
+                icon={FaChartBar}
+                title="Data Analysis"
+                description="Modern experiences optimized for performance, search engines, and converting users to customers."
+              />
+            </FadeInWhenVisible>
+          </WrapItem>
+          <WrapItem>
+            <FadeInWhenVisible>
+              <Card
+                icon={FaUserAstronaut}
+                title="Friendly Interfaces"
+                description="Friendly and accessible interfaces focused on user experience that adapts to all devices."
+              />
+            </FadeInWhenVisible>
+          </WrapItem>
+        </Wrap>
+
       </Section>
       <Section title="Featured Projects">
         <VStack spacing="3rem" w="100%">
