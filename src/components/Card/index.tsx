67ac4a6827dbf8afbc9dbb3699e1256ffd25ee2f
Wilson Franca <wilson.franca.92@gmail.com>
2021-05-20 23:13:11 -0700

    refactor: simplified breakpoints and added them as objects.

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -10,29 +10,29 @@ interface CardProps {
 export function Card({ title, description, icon: CustomIcon }: CardProps) {
     return (
         <VStack
-            spacing={["1rem", "1.5rem"]}
+            spacing={{ base: "1rem", lg: "1.5rem" }}
             bgColor="gray.700"
-            w={["16.75rem", "21.875rem"]}
-            h={["17.25rem", "28.0625rem"]}
+            w={{ base: "16.75rem", lg: "21.875rem" }}
+            h={{ base: "17.25rem", lg: "28.0625rem" }}
             alignItems="center"
             justifyContent="center"
             borderRadius="0.625rem"
             padding="1.5rem"
         >
             <Flex
-                w={["6.25rem", "9.375rem", "10.9375rem", "12.5rem"]}
-                h={["6.25rem", "9.375rem", "10.9375rem", "12.5rem"]}
+                w={{ base: "6.25rem", lg: "12.5rem" }}
+                h={{ base: "6.25rem", lg: "12.5rem" }}
                 borderRadius="50%"
                 alignItems="center"
                 justifyContent="center"
                 background="linear-gradient(136.01deg, #1E1C27 15.64%, rgba(30, 28, 39, 0.38) 83.84%)"
             >
-                <Icon as={CustomIcon} fontSize={["3.125rem", "4.6875rem", "5.5625rem", "6.25rem"]} />
+                <Icon as={CustomIcon} fontSize={{ base: "3.125rem", lg: "6.25rem" }} />
             </Flex>
 
             <Heading
                 fontWeight="medium"
-                fontSize={["1rem", "1.125rem"]}
+                fontSize={{ base: "1rem", lg: "1.125rem" }}
                 lineHeight="100%"
                 letterSpacing="0.05em"
                 textTransform="uppercase"
@@ -43,7 +43,7 @@ export function Card({ title, description, icon: CustomIcon }: CardProps) {
 
             <Text
                 fontWeight="light"
-                fontSize={["0.875rem", "1rem"]}
+                fontSize={{ base: "0.875rem", lg: "1rem" }}
                 lineHeight="140%"
                 letterSpacing="0.04em"
                 maxWidth={250}
@@ -51,6 +51,6 @@ export function Card({ title, description, icon: CustomIcon }: CardProps) {
             >
                 {description}
             </Text>
-        </VStack>
+        </VStack >
     );
 }
