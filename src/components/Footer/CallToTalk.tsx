ef3e74fdc3e4c57033c2e1787f82d8cc63189c0d
Wilson Franca <wilson.franca.92@gmail.com>
2021-05-20 22:41:32 -0700

    refactor: added breakpoints for fontSize.

--- src/components/Footer/CallToTalk.tsx
+++ src/components/Footer/CallToTalk.tsx
@@ -3,8 +3,8 @@ import { VStack, Text } from "@chakra-ui/react";
 export function CallToTalk() {
     return (
         <VStack maxWidth="17.25rem" alignItems="flex-start">
-            <Text fontSize="2.25rem" fontWeight="regular">Interested in collaboration?</Text>
-            <Text fontSize="1.5rem" color="pink.600" fontWeight="regular">Let's Talk</Text>
+            <Text fontSize={{ base: '1.5rem', md: "2.25rem" }} fontWeight="regular">Interested in collaboration?</Text>
+            <Text fontSize={{ base: '1.125rem', md: "1.5rem" }} color="pink.600" fontWeight="regular">Let's Talk</Text>
         </VStack>
     );
 }
