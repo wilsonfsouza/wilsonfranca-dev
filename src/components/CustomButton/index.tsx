7035fefbb906f316446d3a2dcc24dbc5fa362c2e
Wilson Franca <wilson.franca.92@gmail.com>
2021-05-20 22:57:53 -0700

    chore: replaced breakpoints from list to object.

--- src/components/CustomButton/index.tsx
+++ src/components/CustomButton/index.tsx
@@ -1,6 +1,5 @@
 import { Button, ButtonProps } from "@chakra-ui/button";
 import { ReactNode } from "react";
-import { FaTruckMonster } from "react-icons/fa";
 
 interface CustomButtonProps extends ButtonProps {
     children: ReactNode;
@@ -14,7 +13,7 @@ export function CustomButton({ children, isPrimary = true, ...rest }: CustomButt
                 variant="unstyled"
                 bg="#999591"
                 _hover={{ filter: 'brightness(0.95)' }}
-                w={["12.5rem", "15.625rem"]}
+                w={{ base: "12.5rem", lg: "15.625rem" }}
                 h="3.125rem"
                 fontSize="1rem"
                 fontWeight="medium"
@@ -29,7 +28,7 @@ export function CustomButton({ children, isPrimary = true, ...rest }: CustomButt
             variant="unstyled"
             bg="pink.600"
             _hover={{ filter: 'brightness(0.95)' }}
-            w={["12.5rem", "15.625rem"]}
+            w={{ base: "12.5rem", lg: "15.625rem" }}
             h="3.125rem"
             fontSize="1rem"
             fontWeight="medium"
