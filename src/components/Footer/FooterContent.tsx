e61570dadb5fc468d17175c1d2806bfb3a2c362a
Wilson Franca <wilson.franca.92@gmail.com>
2021-05-20 22:31:48 -0700

    refactor: shifted footer content layout between base and md devices.

--- src/components/Footer/FooterContent.tsx
+++ src/components/Footer/FooterContent.tsx
@@ -1,16 +1,33 @@
-import { HStack, useBreakpointValue } from "@chakra-ui/react";
+import { Flex, Stack, useBreakpointValue } from "@chakra-ui/react";
 import { CallToTalk } from "./CallToTalk";
 import { ContactInfoList } from "./ContactInfoList";
 import { FooterNavList } from "./FooterNavList";
 
 export function FooterContent() {
+    const isSmallerScreen = useBreakpointValue({
+        base: true,
+        md: false
+    });
 
+    if (isSmallerScreen) {
+        return (
+            <Flex alignItems="center" justifyContent="center">
+                <Stack direction="column" alignItems="start" justifyContent="center" spacing="2.25rem" width="100%">
+                    <CallToTalk />
+                    <Stack direction="row" alignItems="start" justifyContent="space-evenly" spacing="2.25rem" width="100%">
+                        <FooterNavList />
+                        <ContactInfoList />
+                    </Stack>
+                </Stack>
+            </Flex>
+        );
+    }
 
     return (
-        <HStack alignItems="start" justifyContent="center" spacing={{ base: "2.5rem", lg: "5rem" }} width="100%">
+        <Stack direction="row" alignItems="start" justifyContent="center" spacing="5rem" width="100%">
             <CallToTalk />
             <FooterNavList />
             <ContactInfoList />
-        </HStack>
+        </Stack>
     );
 }
