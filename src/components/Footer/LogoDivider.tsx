26a6cd9cd62973c741c1f2a94a9013ca68975fe8
Wilson Franca <wilson.franca.92@gmail.com>
2021-05-20 22:56:06 -0700

    chore: replaced breakpoints from list to object.

--- src/components/Footer/LogoDivider.tsx
+++ src/components/Footer/LogoDivider.tsx
@@ -3,7 +3,7 @@ import { Logo } from "../Logo";
 
 export function LogoDivider() {
     return (
-        <HStack spacing={["1.5rem", "2.8125rem"]} w="100%" alignItems="center" justifyContent="center" pb="3.625rem">
+        <HStack spacing={{ base: "1.5rem", lg: "2.8125rem" }} w="100%" alignItems="center" justifyContent="center" pb="3.625rem">
             <Logo />
             <Divider orientation="horizontal" height="2px" minWidth="10px" flex="1" color="gray.500" />
         </HStack>
