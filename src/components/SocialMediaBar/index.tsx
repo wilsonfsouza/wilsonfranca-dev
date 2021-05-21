ff20bc0fe527e5e5d31a6e926fefdbf0f60d397b
Wilson Franca <wilson.franca.92@gmail.com>
2021-05-20 21:14:00 -0700

    refactor: extracted github and linkedin navbar into a component.

---
+++ src/components/SocialMediaBar/index.tsx
@@ -0,0 +1,25 @@
+import { Stack, HStack, Icon, StackProps } from "@chakra-ui/react";
+import { ReactNode } from "react";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { ExternalLink } from "../ExternalLink";
+
+interface SocialMediaBarProps extends StackProps {
+    children: ReactNode;
+}
+
+export function SocialMediaBar({ children, ...rest }: SocialMediaBarProps) {
+    return (
+        <Stack direction="row" spacing={["1.5rem", "2.8125rem"]} w="100%" alignItems="center" justifyContent="center" pb="3.625rem" {...rest}>
+            {children}
+            <HStack spacing="1.5rem" alignItems="center" justifyContent="space-between">
+                <ExternalLink href="https://github.com/wilsonfsouza">
+                    <Icon as={FaGithub} fontSize="2.25rem" />
+                </ExternalLink>
+
+                <ExternalLink href="https://www.linkedin.com/in/wilsonfsouza/?locale=en_US">
+                    <Icon as={FaLinkedin} fontSize="2.25rem" />
+                </ExternalLink>
+            </HStack>
+        </Stack>
+    );
+}
