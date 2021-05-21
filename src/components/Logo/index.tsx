17b4896aa9a0c7d3713e4572edac0e2f580c4e8f
Wilson Franca <wilson.franca.92@gmail.com>
2021-05-20 23:34:38 -0700

    refactor: replaced breakpoints lists with objects.

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -8,10 +8,10 @@ interface LogoProps {
 export function Logo({ onClickFunct, isClickable = false }: LogoProps) {
     if (isClickable) {
         return (
-            <Image w={["9.9375rem", "11.375rem", "12.8125rem"]} src="/logo-full.svg" alt="wilfranca" onClick={onClickFunct} style={{ cursor: 'pointer' }} />
+            <Image w={{ base: "9.9375rem", sm: "11.375rem", md: "12.8125rem" }} src="/logo-full.svg" alt="wilfranca" onClick={onClickFunct} style={{ cursor: 'pointer' }} />
         );
     }
     return (
-        <Image w={["9.9375rem", "11.375rem", "12.8125rem"]} src="/logo-full.svg" alt="wilfranca" />
+        <Image w={{ base: "9.9375rem", sm: "11.375rem", md: "12.8125rem" }} src="/logo-full.svg" alt="wilfranca" />
     );
 }
