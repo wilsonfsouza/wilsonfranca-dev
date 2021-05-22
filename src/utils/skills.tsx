18df120567d8befbeff555b496a58e030989c5f2
Wilson Franca <wilson.franca.92@gmail.com>
2021-05-22 14:58:09 -0700

    feat: created a list of skills and respective logos for Who I Am Page.

---
+++ src/utils/skills.tsx
@@ -0,0 +1,70 @@
+import {
+    SiTypescript,
+    SiJavascript,
+    SiReact,
+    SiNextDotJs,
+    SiNodeDotJs,
+    SiPython,
+    SiFigma,
+    SiGit,
+    SiSass,
+    SiMongodb,
+    SiPostgresql,
+    SiDocker,
+    SiJest
+} from 'react-icons/si';
+
+export const skills = [
+    {
+        icon: SiTypescript,
+        title: "TypeScript",
+    },
+    {
+        icon: SiJavascript,
+        title: "JavaScript",
+    },
+    {
+        icon: SiReact,
+        title: "React",
+    },
+    {
+        icon: SiNextDotJs,
+        title: "Next.js",
+    },
+    {
+        icon: SiNodeDotJs,
+        title: "Node.js",
+    },
+    {
+        icon: SiPython,
+        title: "Python",
+    },
+    {
+        icon: SiFigma,
+        title: "Figma",
+    },
+    {
+        icon: SiGit,
+        title: "Git",
+    },
+    {
+        icon: SiMongodb,
+        title: "MongoDB",
+    },
+    {
+        icon: SiPostgresql,
+        title: "PostgreSQL",
+    },
+    {
+        icon: SiSass,
+        title: "SASS",
+    },
+    {
+        icon: SiDocker,
+        title: "Docker",
+    },
+    {
+        icon: SiJest,
+        title: "Jest",
+    },
+]
