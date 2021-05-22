c637f72d8cdcfc8a5e2ea88b1cc6cc168e980bc4
Wilson Franca <wilson.franca.92@gmail.com>
2021-05-22 15:37:16 -0700

    refactor: used useBreakpointValue hook to modify the lenght of the pink bar after the title for smaller devices.

--- src/components/Section/SectionTitle.tsx
+++ src/components/Section/SectionTitle.tsx
@@ -1,20 +1,44 @@
-import { Heading } from "@chakra-ui/react";
+import { Heading, useBreakpointValue } from "@chakra-ui/react";
 
 interface SectionTitleProps {
     content: string;
 }
 
 export function SectionTitle({ content }: SectionTitleProps) {
+    const isMobile = useBreakpointValue({
+        base: true,
+        md: false
+    })
+
+    if (isMobile) {
+        return (
+            <Heading
+                fontSize={{ base: "2rem", sm: "2.5rem" }}
+                position="relative"
+                height="fit-content"
+                _after={{
+                    content: '""',
+                    position: 'absolute',
+                    height: '2px',
+                    width: '3.375rem',
+                    background: 'pink.600',
+                    left: '0',
+                    bottom: '-0.6875rem'
+                }}
+            >{content}</Heading>
+        );
+    }
+
     return (
         <Heading
-            fontSize={{ base: "2rem", sm: "3rem", md: "4rem" }}
+            fontSize={{ md: "3.5rem" }}
             position="relative"
             height="fit-content"
             _after={{
                 content: '""',
                 position: 'absolute',
                 height: '2px',
-                width: '6.875rem',
+                width: '6.375rem',
                 background: 'pink.600',
                 left: '0',
                 bottom: '-0.6875rem'
