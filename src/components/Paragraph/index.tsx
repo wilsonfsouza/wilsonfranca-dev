163a9fefd4ed8b48ba48bbd78fa060769616b0bb
Wilson Franca <wilson.franca.92@gmail.com>
2021-05-22 15:24:27 -0700

    feat: reduzed fontSize.

--- src/components/Paragraph/index.tsx
+++ src/components/Paragraph/index.tsx
@@ -8,9 +8,8 @@ export function Paragraph({ children }: ParagraphProps) {
     return (
         <Text
             fontWeight="light"
-            fontSize={{ base: "1rem", sm: "1.5rem", md: "1.875rem", lg: "2.25rem" }}
+            fontSize={{ base: "1rem", sm: "1.375rem", md: "1.75rem" }}
             letterSpacing={{ base: "0.1rem", sm: "0.15rem", md: "0.1875rem", lg: "0.225rem" }}
-            maxWidth="90%"
         >
             {children}
         </Text>
