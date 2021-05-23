2e9fe561d9ee43330a7f621029c95d636afc3acd
Wilson Franca <wilson.franca.92@gmail.com>
2021-05-22 21:06:10 -0700

    chore: modified icons for email and phone.

--- src/components/Footer/ContactInfoList.tsx
+++ src/components/Footer/ContactInfoList.tsx
@@ -1,14 +1,14 @@
 import { Text, VStack } from "@chakra-ui/react";
-import { FaMailBulk, FaPhone } from "react-icons/fa";
+import { IoIosMail, IoMdPhonePortrait } from "react-icons/io";
 import { ContactInfoItem } from "./ContactInfoItem";
 
 export function ContactInfoList() {
     return (
         <VStack alignItems="flex-start" spacing="1.5rem">
-            <ContactInfoItem icon={FaPhone}>
+            <ContactInfoItem icon={IoMdPhonePortrait}>
                 <Text fontFamily="Roboto Slab" fontSize={{ base: "0.875rem", lg: "1.125rem" }} fontWeight="light">+1 (530) 782-0830</Text>
             </ContactInfoItem>
-            <ContactInfoItem icon={FaMailBulk}>
+            <ContactInfoItem icon={IoIosMail}>
                 <Text fontFamily="Roboto Slab" fontSize={{ base: "0.875rem", lg: "1.125rem" }} fontWeight="bold">wilson.franca.92</Text>
                 <Text fontFamily="Roboto Slab" fontSize={{ base: "0.875rem", lg: "1.125rem" }} fontWeight="light">@gmail.com</Text>
             </ContactInfoItem>
