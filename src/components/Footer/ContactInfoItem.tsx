62a113a38c84305c2aa022b7e82de58856f54b37
Wilson Franca <wilson.franca.92@gmail.com>
2021-05-22 21:11:39 -0700

    refactor: extended StackProps to allow changing direction in contact page.

--- src/components/Footer/ContactInfoItem.tsx
+++ src/components/Footer/ContactInfoItem.tsx
@@ -1,15 +1,15 @@
-import { Stack, VStack, Icon } from "@chakra-ui/react";
+import { Stack, VStack, Icon, StackProps } from "@chakra-ui/react";
 import { ReactNode } from "react";
 import { IconBaseProps } from "react-icons";
 
-interface ContactInfoItemProps {
+interface ContactInfoItemProps extends StackProps {
     icon: React.ComponentType<IconBaseProps>;
     children: ReactNode;
 }
 
-export function ContactInfoItem({ children, icon: CustomIcon }: ContactInfoItemProps) {
+export function ContactInfoItem({ children, icon: CustomIcon, ...rest }: ContactInfoItemProps) {
     return (
-        <Stack direction={{ base: "column", lg: "row" }} alignItems="center" justifyContent="center" spacing="1.5rem">
+        <Stack direction={{ base: "column", lg: "row" }} alignItems="center" justifyContent="center" spacing="1.5rem" {...rest}>
             <Icon as={CustomIcon} fontSize={{ base: "1.5rem", lg: "1.875rem" }} />
             <VStack alignItems="start" spacing="0">
                 {children}
