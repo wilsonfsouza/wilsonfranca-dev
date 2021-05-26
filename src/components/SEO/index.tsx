b0edd000987a7d71e6e80b45f6c2ffbaac117254
Wilson Franca <wilson.franca.92@gmail.com>
2021-05-25 21:55:02 -0700

    fix: seo tags for social media.

--- src/components/SEO/index.tsx
+++ src/components/SEO/index.tsx
@@ -10,22 +10,29 @@ export function SEO({ title }: SEOProps) {
             <title>wilfranca | {title}</title>
 
             <link rel="shortcut icon" href="/favicons/logo-short-pink.png" />
+            <meta name="title" content={`"wilfranca | ${title}"`} />
             <meta name="keywords" content="wilsonfsouza, software developer, portfolio" />
             <meta name="description" content="Wilson Franca de Souza - Software Developer and UI/UX enthusiast based in Redding, CA (US)." />
 
+            {/* <!-- Open Graph / Facebook --> */}
+            <meta property="og:type" content="website" />
+            <meta property="og:url" content={process.env.NEXT_PUBLIC_WEBSITE_URL} />
             <meta property="og:site_name" content="wilfranca" />
-            <meta property="og:title" content="wilfranca" />
+            <meta property="og:title" content={`"wilfranca | ${title}"`} />
             <meta property="og:description" content="Wilson Franca de Souza - Software Developer and UI/UX enthusiast based in Redding, CA (US)." />
             <meta property="og:image" content="/thumb.svg" />
             <meta property="og:image:type" content="image/svg" />
             <meta property="og:image:width" content="1200" />
             <meta property="og:image:height" content="630" />
 
+            {/* <!-- Twitter --> */}
+            <meta name="twitter:card" content="summary_large_image" />
+            <meta property="twitter:url" content={process.env.NEXT_PUBLIC_WEBSITE_URL} />
+            <meta property="twitter:title" content={`"wilfranca | ${title}"`} />
+            <meta name="twitter:description" content="Wilson Franca de Souza - Software Developer and UI/UX enthusiast based in Redding, CA (US)." />
             <meta name="twitter:image" content="/thumb.svg" />
             <meta name="twiiter:image:alt" content="Thumbnail" />
-            <meta name="twitter:card" content="summary_large_image" />
             <meta name="twitter:title" content="wilfranca" />
-            <meta name="twitter:description" content="Wilson Franca de Souza - Software Developer and UI/UX enthusiast based in Redding, CA (US)." />
             <meta name="twiiter:create" content="wilsonfsouza" />
         </Head>
     );
