d63eb5271412167e92235b6c00f4c110f4217a7c
Wilson Franca <wilson.franca.92@gmail.com>
2021-05-25 21:59:33 -0700

    fix: removed quotation marks from seo title tags for social media.

--- src/components/SEO/index.tsx
+++ src/components/SEO/index.tsx
@@ -10,7 +10,7 @@ export function SEO({ title }: SEOProps) {
             <title>wilfranca | {title}</title>
 
             <link rel="shortcut icon" href="/favicons/logo-short-pink.png" />
-            <meta name="title" content={`"wilfranca | ${title}"`} />
+            <meta name="title" content={`wilfranca | ${title}`} />
             <meta name="keywords" content="wilsonfsouza, software developer, portfolio" />
             <meta name="description" content="Wilson Franca de Souza - Software Developer and UI/UX enthusiast based in Redding, CA (US)." />
 
@@ -18,7 +18,7 @@ export function SEO({ title }: SEOProps) {
             <meta property="og:type" content="website" />
             <meta property="og:url" content={process.env.NEXT_PUBLIC_WEBSITE_URL} />
             <meta property="og:site_name" content="wilfranca" />
-            <meta property="og:title" content={`"wilfranca | ${title}"`} />
+            <meta property="og:title" content={`wilfranca | ${title}`} />
             <meta property="og:description" content="Wilson Franca de Souza - Software Developer and UI/UX enthusiast based in Redding, CA (US)." />
             <meta property="og:image" content="/thumb.svg" />
             <meta property="og:image:type" content="image/svg" />
@@ -28,7 +28,7 @@ export function SEO({ title }: SEOProps) {
             {/* <!-- Twitter --> */}
             <meta name="twitter:card" content="summary_large_image" />
             <meta property="twitter:url" content={process.env.NEXT_PUBLIC_WEBSITE_URL} />
-            <meta property="twitter:title" content={`"wilfranca | ${title}"`} />
+            <meta property="twitter:title" content={`wilfranca | ${title}`} />
             <meta name="twitter:description" content="Wilson Franca de Souza - Software Developer and UI/UX enthusiast based in Redding, CA (US)." />
             <meta name="twitter:image" content="/thumb.svg" />
             <meta name="twiiter:image:alt" content="Thumbnail" />
