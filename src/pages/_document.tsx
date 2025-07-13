f33aaf75afea24c3d8fb7081f1764fe9ceb7ea61
Wilson Franca <wilson.franca.92@gmail.com>
2025-07-12 17:11:52 -0700

    fix(_document): remove render method to match next js version

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,19 +1,17 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { Html, Head, Main, NextScript } from 'next/document';
 
-export default class MyDocument extends Document {
-    render() {
-        return (
-            <Html lang="en">
-                <Head>
-                    <link rel="preconnect" href="https://fonts.gstatic.com" />
-                    <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@300;400;500;700&family=Roboto:wght@300;500&display=swap" rel="stylesheet" />
-                    <link rel="shortcut icon" type="image/png" href="/favicons/logo-short-pink.png" />
-                </Head>
-                <body>
-                    <Main />
-                    <NextScript />
-                </body>
-            </Html>
-        )
-    }
+export default function Document() {
+    return (
+        <Html lang="en">
+            <Head>
+                <link rel="preconnect" href="https://fonts.gstatic.com" />
+                <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@300;400;500;700&family=Roboto:wght@300;500&display=swap" rel="stylesheet" />
+                <link rel="shortcut icon" type="image/png" href="/favicons/logo-short-pink.png" />
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    )
 }
