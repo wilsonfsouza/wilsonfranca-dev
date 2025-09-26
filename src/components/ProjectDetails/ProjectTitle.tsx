b5b50dd9bc41845751352ef79d4faac906d28178
Wilson Franca <wilson.franca.92@gmail.com>
2025-09-26 08:28:43 -0700

    fix(project-title): change h2 to h3 for semantic html since the section title is h2

--- src/components/ProjectDetails/ProjectTitle.tsx
+++ src/components/ProjectDetails/ProjectTitle.tsx
@@ -8,6 +8,7 @@ interface ProjectTitleProps {
 export function ProjectTitle({ children }: ProjectTitleProps) {
   return (
     <Heading
+      as="h3"
       fontWeight="medium"
       fontSize={{ base: "1.125rem", sm: "1.6875rem", md: "2.25rem" }}
       lineHeight={{ base: "2.24rem", md: "2.7rem" }}
