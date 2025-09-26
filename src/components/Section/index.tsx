49c38cfaf2a2ff3d15199169fd366c851dd3831d
Wilson Franca <wilson.franca.92@gmail.com>
2025-09-26 08:26:17 -0700

    fix: remove back button since it wasn't necessary

--- src/components/Section/index.tsx
+++ src/components/Section/index.tsx
@@ -1,21 +1,14 @@
-import { Button, Flex, FlexProps, HStack } from "@chakra-ui/react";
+import { Flex, FlexProps, HStack } from "@chakra-ui/react";
 import { ReactNode } from "react";
 import { SectionTitle } from "./SectionTitle";
 
 interface SectionProps extends FlexProps {
   children: ReactNode;
   title: string;
-  hasBackButton?: boolean;
   goBack?: () => void;
 }
 
-export function Section({
-  title,
-  children,
-  hasBackButton = false,
-  goBack,
-  ...rest
-}: SectionProps) {
+export function Section({ title, children, goBack, ...rest }: SectionProps) {
   return (
     <Flex
       as="section"
@@ -28,18 +21,6 @@ export function Section({
       <Flex h="100%" w="100%" mx="auto" maxWidth={1480} direction="column">
         <HStack justifyContent="space-between" alignItems="center">
           <SectionTitle content={title} />
-          {hasBackButton && (
-            <Button
-              variant="plain"
-              fontFamily="Roboto Slab"
-              fontSize={{ base: "1rem", sm: "1.375rem", md: "1.75rem" }}
-              fontWeight="light"
-              _hover={{ textDecoration: "underline", color: "pink.600" }}
-              onClick={goBack}
-            >
-              Back
-            </Button>
-          )}
         </HStack>
         <Flex
           as="div"
