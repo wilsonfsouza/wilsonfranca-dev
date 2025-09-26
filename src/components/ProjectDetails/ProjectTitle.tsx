a971462852ed852e6a62ce11363ea4c773be96a3
Wilson Franca <wilson.franca.92@gmail.com>
2025-09-26 08:25:38 -0700

    fix: correct line height of headings on project details

--- src/components/ProjectDetails/ProjectTitle.tsx
+++ src/components/ProjectDetails/ProjectTitle.tsx
@@ -2,11 +2,17 @@ import { Heading } from "@chakra-ui/react";
 import { ReactNode } from "react";
 
 interface ProjectTitleProps {
-    children: ReactNode;
+  children: ReactNode;
 }
 
 export function ProjectTitle({ children }: ProjectTitleProps) {
-    return (
-        <Heading fontWeight="medium" fontSize={{ base: "1.125rem", sm: "1.6875rem", md: "2.25rem" }}>{children}</Heading>
-    );
+  return (
+    <Heading
+      fontWeight="medium"
+      fontSize={{ base: "1.125rem", sm: "1.6875rem", md: "2.25rem" }}
+      lineHeight={{ base: "2.24rem", md: "2.7rem" }}
+    >
+      {children}
+    </Heading>
+  );
 }
